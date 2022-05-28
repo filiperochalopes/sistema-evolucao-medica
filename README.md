d9dcd7c98777328c5d5b0aeae22d8877348a5faf
Filipe Lopes <filiperocklopes@gmail.com>
2022-05-28 20:53:17 -0300

    start to add some login rules, models and routes. Deleted unused files

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+# O que é um MAPA?
+
+MAPA de pacientes é uma ferramenta onde colocamos de forma resumida os dados relevantes dos paciente internados para melhorar o seguimento do cuidado.
+
+# Sobre o sistema
+
+O sistema foi criado para viabilizar um mapa simples, como um projeto piloto pessoal para minha prática médiga em uma Policlínica 
+
+### Gerando secret key
+
+```sh
+python -c 'import secrets; print(secrets.token_hex())'
+```
+
 ```
 docker-compose up
+```
+
+```sh
+flask db init
+flask db migrate -m "Initial migration"
+flask db upgrade
 ```
--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -1,58 +1,39 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from app.routes.auth import auth as auth_blueprint
+from app.routes.main import main as main_blueprint
+from flask import Flask
 from flask_migrate import Migrate
 from flask_scss import Scss
+from flask_bcrypt import Bcrypt
+from app.models import db
+from app.models import User
 
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-
-import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite3.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 migrate = Migrate(app, db)
 Scss(app, static_dir='app/static/css', asset_dir='app/assets/scss')
 
-class Service(db.Model):
-    __tablename__ = 'services'
+app.register_blueprint(main_blueprint)
+app.register_blueprint(auth_blueprint)
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
-    parent = relationship("Service", back_populates="services")
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-class StatusRecord(db.Model):
-    __tablename__ = 'status_records'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
-@app.route('/')
-def index():
-    return render_template('intubacao_sedacao_ga.html')
-
-@app.route('/intubacao')
-def intubacao():
-    return render_template('intubacao_sedacao_ga.html')
-
-@app.route('/doses-pediatricas')
-def doses_pediatricas():
-    return render_template('doses_pediatricas.html')
-
-@app.route('/5h5t')
-def aesp():
-    return render_template('5h5t.html')
-
-@app.route('/trechos-exame')
-def trechos():
-    return render_template('trechos.html')
+@app.cli.command("seed")
+def seed():
+    """Seed the database."""
+    user_1 = User(username="medico", password="senha@123")
+    db.session.add(user_1)
+    db.session.commit()
+    print(user_1)
