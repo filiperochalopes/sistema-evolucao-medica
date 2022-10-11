07df6815018ee27ac7c9e5b16de3966fdb438732
ckk3 <contato.gustavomota@gmil.com>
2022-10-11 18:31:22 -0300

    add document datetime

--- flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
@@ -10,7 +10,7 @@ import global_functions
 template_directory = "./graphql/mutations/pdfs/pdfs_templates/ficha_de_internamento_hmlem.pdf"
 
 
-def fill_pdf_ficha_internamento(datetime:datetime, patient_name:str, patient_cns:int, patient_birthday:datetime):
+def fill_pdf_ficha_internamento(datetime:datetime.datetime, patient_name:str, patient_cns:int, patient_birthday:datetime.datetime):
 
     try:
 
@@ -18,11 +18,17 @@ def fill_pdf_ficha_internamento(datetime:datetime, patient_name:str, patient_cns
         #Create canvas and add data
         c = canvas.Canvas(packet, pagesize=letter)
         #Change canvas font to mach with the document
+        #this is also changed in the document to some especific fields
         c.setFont('Helvetica', 9)
         #Writing all data in respective fields
         try:
             c = add_patientName(canvas=c, name=patient_name)
             c = add_patientCNS(canvas=c, cns=patient_cns)
+            #change font size to datetime
+            c.setFont('Helvetica', 14)
+            c = add_documentDatetime(canvas=c, datetime=datetime)
+            c.setFont('Helvetica', 9)
+
             #verify if c is a error at some point
             if type(c) == type(Response()):
                 return c
@@ -48,6 +54,7 @@ def fill_pdf_ficha_internamento(datetime:datetime, patient_name:str, patient_cns
     except:
         return Response("Error while filling ficha de internamento", status=500)
 
+
 def add_patientName(canvas:canvas.Canvas, name:str):
     """Add patient name to pdf
 
@@ -68,7 +75,17 @@ def add_patientName(canvas:canvas.Canvas, name:str):
     except:
         return Response('Unknow error while adding patient name', status=500)
 
+
 def add_patientCNS(canvas:canvas.Canvas, cns:int):
+    """Add patient cns to document
+
+    Args:
+        canvas (canvas.Canvas): canvas to use
+        cns (int): patient cns
+
+    Returns:
+        canvas or Response:canvas if everthing is allright or Response if hapens some error 
+    """    
     try:
         #Verify id the cns is valid
         if global_functions.isCNSvalid(cns):
@@ -81,6 +98,24 @@ def add_patientCNS(canvas:canvas.Canvas, cns:int):
             return Response("Unable to add patient cns because is a invalid CNS", status=400)
     except:
         return Response('Unknow error while adding patient cns', status=500)
+
+
+def add_documentDatetime(canvas:canvas.Canvas, datetime:datetime.datetime):
+    """Add document datetime to pdf
+
+    Args:
+        canvas (canvas.Canvas): canvas 
+        datetime (datetime.datetime): datetime to add
+    Returns:
+        canvas or Response:canvas if everthing is allright or Response if hapens some error 
+    """    
+    try:
+        #Format datetime to format DD/MM/YYYY H:M:S
+        datetime = datetime.strftime("%m/%d/%Y %H:%M:%S")
+        canvas = add_data(canvas=canvas, data=datetime, pos=(415, 741))
+        return canvas
+    except:
+        return Response('Unkown error while adding document datetime')
 
 
 def add_data(canvas:canvas.Canvas, data:str, pos:tuple):
