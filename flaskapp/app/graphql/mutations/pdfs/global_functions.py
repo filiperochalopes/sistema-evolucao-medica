5b7f582eee5cc38ca1d48875be1dd88a9a6c6c02
ckk3 <contato.gustavomota@gmil.com>
2022-10-11 20:37:10 -0300

    add cpf and rg validator en add in document

--- flaskapp/app/graphql/mutations/pdfs/global_functions.py
+++ flaskapp/app/graphql/mutations/pdfs/global_functions.py
@@ -1,4 +1,4 @@
-
+import re
 
 def isCNSvalid(cns:int) -> bool:
     """verify if the CNS is valid
@@ -18,9 +18,50 @@ def isCNSvalid(cns:int) -> bool:
 
 
 
-if __name__ == "__main__":
-    cns = 138490784180008
-    cns = str(cns)
-    cns = cns[:3] + " " + cns[3:7] + " " + cns[7:11] + " " + cns[11:15]
+def isRGvalid(rg:int) -> bool:
+    # Notice that RG changes a lot in every brazillian state
+    # so theres a chance that a invalid RG has pass as Valid
+    # just because RG is matematician valid dont mean that exists in government database
+    #the only verification that can do is the maximum value
+    return len(str(rg)) <= 16
 
-    print(cns)
+def isCPFvalid(cpf: str) -> bool:
+    """Verify if the CPF is valid
+
+    Args:
+        cpf (str): cpf to be validated
+
+    Returns:
+        bool: true or false
+    """    
+
+    # Verify format
+    if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+        return False
+
+    # receive only numbers
+    numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+    # See if all numbers are equal or longer than 11 digits
+    if len(numbers) != 11 or len(set(numbers)) == 1:
+        return False
+
+    # Validate first verificator digit
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[9] != expected_digit:
+        return False
+
+    # Validate second verificator digit
+
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[10] != expected_digit:
+        return False
+    return True
+
+if __name__ == "__main__":
+    cpf = '131.324.345.11'
+    print(isCPFvalid(cpf))
+
+    
--- flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
@@ -10,36 +10,41 @@ import global_functions
 template_directory = "./graphql/mutations/pdfs/pdfs_templates/ficha_de_internamento_hmlem.pdf"
 
 
-def fill_pdf_ficha_internamento(documentDatetime:datetime.datetime, patient_name:str, patient_cns:int, patient_birthday:datetime.datetime, patient_motherName:str):
+def fill_pdf_ficha_internamento(documentDatetime:datetime.datetime, patient_name:str, patient_cns:int, patient_birthday:datetime.datetime, patient_motherName:str, patient_document:dict):
 
     try:
 
         packet = io.BytesIO()
-        #Create canvas and add data
+        # Create canvas and add data
         c = canvas.Canvas(packet, pagesize=letter)
-        #Change canvas font to mach with the document
-        #this is also changed in the document to some especific fields
+        # Change canvas font to mach with the document
+        # this is also changed in the document to some especific fields
         c.setFont('Helvetica', 9)
-        #Writing all data in respective fields
+        # Writing all data in respective fields
+        # not null data
         try:
             c = add_patientName(canvas=c, name=patient_name)
             c = add_patientCNS(canvas=c, cns=patient_cns)
-            #change font size to datetime
+            # change font size to datetime
             c.setFont('Helvetica', 14)
             c = add_documentDatetime(canvas=c, datetime=documentDatetime)
             c.setFont('Helvetica', 9)
             c = add_patientBirthday(canvas=c, birthday=patient_birthday)
             c = add_patientMotherName(canvas=c, motherName=patient_motherName)
+            c = add_patientDocument(canvas=c, document=patient_document)
 
-            #verify if c is a error at some point
+            # verify if c is a error at some point
             if type(c) == type(Response()):
                 return c
+            elif type(c) == type(None):
+                return Response('Some error happen when adding not null data do fields', status=500)
         except:
             if type(c) == type(Response()):
                 return c
             else:
-                return Response('Some error happen when adding data to fields', status=500)
+                return Response('Some error happen when adding not null data to fields', status=500)
         # create a new PDF with Reportlab
+        print('chegou aqui')
         c.save()
         packet.seek(0)
         new_pdf = PdfReader(packet)
@@ -68,7 +73,7 @@ def add_patientName(canvas:canvas.Canvas, name:str):
         canvas or Response:canvas if everthing is allright or Response if hapens some error 
     """    
     try:
-        #verify if patient name is smaller than 60 characters
+        # verify if patient name is smaller than 60 characters
         if len(name.strip()) <= 60:
             canvas = add_data(canvas=canvas, data=name, pos=(27, 673))
             return canvas
@@ -89,9 +94,9 @@ def add_patientCNS(canvas:canvas.Canvas, cns:int):
         canvas or Response:canvas if everthing is allright or Response if hapens some error 
     """    
     try:
-        #Verify id the cns is valid
+        # Verify if the cns is valid
         if global_functions.isCNSvalid(cns):
-            #format cns to add in document
+            # format cns to add in document
             cns = str(cns)
             cns = cns[:3] + " " + cns[3:7] + " " + cns[7:11] + " " + cns[11:15]
             canvas = add_data(canvas=canvas, data=cns, pos=(434, 674))
@@ -112,7 +117,7 @@ def add_documentDatetime(canvas:canvas.Canvas, datetime:datetime.datetime):
         canvas or Response:canvas if everthing is allright or Response if hapens some error 
     """    
     try:
-        #Format datetime to format DD/MM/YYYY H:M:S
+        # Format datetime to format DD/MM/YYYY H:M:S
         datetime = datetime.strftime("%m/%d/%Y %H:%M:%S")
         canvas = add_data(canvas=canvas, data=datetime, pos=(415, 741))
         return canvas
@@ -131,7 +136,7 @@ def add_patientBirthday(canvas:canvas.Canvas, birthday:datetime.datetime):
         canvas or Response:canvas if everthing is allright or Response if hapens some error 
     """    
     try:
-        #Format birthday to format DD/MM/YYYY
+        # Format birthday to format DD/MM/YYYY
         birthday = birthday.strftime("%m/%d/%Y")
         canvas = add_data(canvas=canvas, data=birthday, pos=(43, 643))
         return canvas
@@ -150,7 +155,7 @@ def add_patientMotherName(canvas:canvas.Canvas, motherName:str):
         canvas or Response:canvas if everthing is allright or Response if hapens some error 
     """    
     try:
-        #verify if patient motherName is smaller than 60 characters
+        # verify if patient motherName is smaller than 60 characters
         if len(motherName.strip()) <= 60:
             canvas = add_data(canvas=canvas, data=motherName, pos=(194, 643))
             return canvas
@@ -158,6 +163,39 @@ def add_patientMotherName(canvas:canvas.Canvas, motherName:str):
             return Response("Unable to add patient motherName because is longer than 60 characters", status=400)
     except:
         return Response('Unknow error while adding patient motherName', status=500)
+
+
+def add_patientDocument(canvas:canvas.Canvas, document:dict):
+    """Add patient document, CPF or Rg
+
+    Args:
+        canvas (canvas.Canvas): canvas to use
+        document (dict): dict with format {"doc":number}
+    Returns:
+        canvas or Response:canvas if everthing is allright or Response if hapens some error 
+    """    
+    try:
+        # See id document is CPF or RG
+        if 'RG' in document.keys():
+            #The only verificatinon is that rg is not greater than 16 characteres
+            if global_functions.isRGvalid(document['RG']):
+                canvas = add_data(canvas=canvas, data=str(document['RG']), pos=(92, 610))
+                return canvas
+            else:
+                return Response('Patient RG is not valid', status=400)
+        elif 'CPF' in document.keys():
+            #Format cpf to validate
+            cpf = str(document['CPF'])
+            cpf = cpf[:3] + "." + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
+            if global_functions.isCPFvalid(cpf):
+                canvas = add_data(canvas=canvas, data=cpf, pos=(92, 610))
+                return canvas
+            else:
+                return Response('Patient CPF is not valid', status=400)
+        else:
+            Response('The document was not CPF or RG', status=400)
+    except:
+        Response('Unknow error while adding patient Document', status=500)
 
 
 def add_data(canvas:canvas.Canvas, data:str, pos:tuple):
@@ -205,7 +243,8 @@ if __name__ == "__main__":
         patient_name="Patient Name",
         patient_cns=928976954930007,
         patient_birthday=datetime.datetime.now(),
-        patient_motherName="Patient Mother Name"
+        patient_motherName="Patient Mother Name",
+        patient_document={'CPF':28445400070}
         )
     if type(output) == type(Response()): 
         print(output.response)
