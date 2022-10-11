1661182adb9c422ad454b8f0366e6d82e2863d74
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-11 15:40:30 -0300

    feat(page): add initial List Pacient

---
+++ flaskapp/app/templates/reactapp/src/views/Pacient/List/index.jsx
@@ -0,0 +1,41 @@
+import Container, { PacientContent } from "./styles";
+
+import Button from "components/Button";
+import Pacient from "components/Pacient";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const List = () => (
+  <Container>
+    <Link to="/">
+      <Button className="add_new_pacient">+ Admitir paciente</Button>
+    </Link>
+
+    <div className="pacients-container">
+      <h2>Pacientes Internados</h2>
+      <div className="pacients">
+        <Pacient>
+          <PacientContent>
+            <p>Nome Completo, masculino, 80 anos</p>
+            <div className="container_buttons">
+              <Button className="add_new_pacient" customType="gray">
+                Visualizar
+              </Button>
+              <div className="container_button_relative">
+                <Button className="add_new_pacient" customType="gray">
+                  Evoluir
+                </Button>
+              </div>
+              <Button className="add_new_pacient" customType="gray">
+                Imprimir
+              </Button>
+            </div>
+          </PacientContent>
+        </Pacient>
+      </div>
+    </div>
+    <Button className="config">Configurações</Button>
+  </Container>
+);
+
+export default List;
---
+++ flaskapp/app/templates/reactapp/src/views/Pacient/List/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 2.625rem;
+  margin-bottom: 2rem;
+  flex: 1;
+  align-self: stretch;
+
+  .add_new_pacient {
+    max-width: 10.5rem;
+  }
+
+  .config {
+    max-width: 8.75rem;
+    align-self: flex-end;
+  }
+
+  .pacients-container {
+    margin-top: 2rem;
+    flex: 1;
+
+    h2 {
+      font-size: 1.5rem;
+      margin-bottom: 2rem;
+    }
+
+    .pacients {
+      display: flex;
+      flex-direction: column;
+      row-gap: 0.5rem;
+    }
+  }
+`;
+
+export const PacientContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .container_buttons {
+    display: flex;
+    column-gap: 0.625rem;
+
+    .container_button_relative {
+      position: relative;
+    }
+  }
+`;
