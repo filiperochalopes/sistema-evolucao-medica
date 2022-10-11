2a8fe22540c9654d58ef497c73fa939a91f45a6f
ckk3 <contato.gustavomota@gmil.com>
2022-10-11 13:46:01 -0300

    primeira adicao de nome

--- flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_ficha_internamento.py
@@ -3,20 +3,17 @@ from PyPDF2  import PdfFileWriter, PdfFileReader
 import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import mm
 
 template_directory = "./graphql/mutations/pdfs/pdfs_templates/ficha_de_internamento_hmlem.pdf"
 
-def fill_pdf_ficha_internamento(datetime:datetime, patient_name:str, patient_cns:int, patient_birthday:datetime):
-    pass
 
-def hello_word(name="name"):
+def fill_pdf_ficha_internamento(datetime:datetime, patient_name:str, patient_cns:int, patient_birthday:datetime):
 
     packet = io.BytesIO()
     #Create canvas and add data
     c = canvas.Canvas(packet, pagesize=letter)
-    c.drawString(100, 100, "TESTANTO AOOBA")
-    c = add_data(canvas=c, data="OUTRO TESTE", pos=(0, 100))
+    #Writing all data in respective fields
+    c = add_data(canvas=c, data=patient_name, pos=(27, 673))
 
     # create a new PDF with Reportlab
     c.save()
@@ -39,7 +36,7 @@ def add_data(canvas:canvas.Canvas, data:str, pos:tuple):
     Args:
         canvas (canvas.Canvas): canvas that will be used to add data
         data (str): data to be added
-        pos (tuple): data insert position in milimeters (mm)
+        pos (tuple): data insert position in points
 
     Returns:
         canvas(canvas.Canvas): canvas with all changes
@@ -61,6 +58,13 @@ def write_newpdf(newpdf:PdfFileWriter, new_directory:str):
 
 
 if __name__ == "__main__":
-    output = hello_word()
+    output = fill_pdf_ficha_internamento(
+        datetime=datetime.datetime.now(), 
+        patient_name="Patient Name",
+        patient_cns=928976954930007,
+        patient_birthday=datetime.datetime.now()
+        )
+    
+    
     write_newpdf(output, "./graphql/mutations/pdfs/ficha_teste.pdf")
     
