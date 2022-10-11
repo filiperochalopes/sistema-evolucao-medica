0af8596ee596327566cd331aaff90d68060a969a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-11 15:36:38 -0300

    feat(component): create pacient component

---
+++ flaskapp/app/templates/reactapp/src/components/Pacient/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100%;
+  height: 2.5rem;
+  background-color: ${({ theme }) => theme.colors.gray};
+  padding: 0.5em 1.125rem;
+`;
