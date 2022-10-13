b9726a2c80c2477d7898e7974e17b50e2fb8cf80
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-13 09:17:30 -0300

    feat(view): finished design of evolution view

--- flaskapp/app/templates/reactapp/src/views/Pacient/Evolution/index.jsx
+++ flaskapp/app/templates/reactapp/src/views/Pacient/Evolution/index.jsx
@@ -7,36 +7,65 @@ import TextArea from "components/TextArea";
 
 const Evolution = () => (
   <Container>
-    <div>
-      <div>
+    <div className="evolution_pacient">
+      <div className="header">
         <h2>Evoluir Paciente (João Miguel dos Santos Polenta, 83 anos)</h2>
         <Button>Atualizar Dados do Paciente</Button>
       </div>
       <TextArea placeholder="EVOLUÇÃO" />
       <div className="row">
-        <Input placeholder="RESPONSÁVEL PELA EVOLUÇÃO (NOME COMPLETO)" />
-        <Input placeholder="FUNÇÃO" />
-        <Input placeholder="N° CONSELHO" />
+        <Input
+          placeholder="RESPONSÁVEL PELA EVOLUÇÃO (NOME COMPLETO)"
+          className="larger"
+        />
+        <Input placeholder="FUNÇÃO" className="normal" />
+        <Input placeholder="N° CONSELHO" className="small" />
       </div>
-      <Input placeholder="CID - SUSPEITA INICIAL" />
+      <div>
+        <Input placeholder="CID - SUSPEITA INICIAL" />
+        <p className="legend">
+          ÚLTIMA EVOLUÇÃO ATUALIZADA POR FULANO EM DD/MM/AAAA HH:DD
+        </p>
+      </div>
 
-      <Button>Evoluir</Button>
+      <Button className="evolution_button">Evoluir</Button>
     </div>
 
-    <div>
+    <div className="prescriptions_pacient">
       <h2>Prescrição</h2>
       <ol>
         <li>
           <ListOption>REPOUSO RELATIVO NO LEITO À 30°</ListOption>
         </li>
+        <li>
+          <ListOption>REPOUSO RELATIVO NO LEITO À 30°</ListOption>
+        </li>
+        <li>
+          <ListOption>REPOUSO RELATIVO NO LEITO À 30°</ListOption>
+          <p className="error">
+            PARA CADASTRAR UM ANTIBIÓTICO É NECESSÁRIO COLOCAR QUAL FOI O DIA
+            INICIAL DE USO ESCREVENDO “D0 12/08/2022”
+          </p>
+        </li>
       </ol>
-      <div></div>
-      <Button>Atualizar prescrição</Button>
+      <p className="legend">ÚLTIMA PRESCRIÇÃO ATUALIZADA EM DD/MM/AAAA HH:DD</p>
+      <div className="buttons">
+        <Button className="button_add_prescription">
+          Adicionar medicações sintomáticas
+        </Button>
+        <Button className="button_normal">Adicionar nova linha</Button>
+      </div>
+      <Button className="button_normal button-update_prescription">
+        Atualizar prescrição
+      </Button>
     </div>
-    <div>
+    <div className="pendencies_pacient">
       <h2>Pendências</h2>
       <TextArea placeholder="AQUI SEGUE UM TEXTO COM AS PENDÊNCIAS"></TextArea>
-      <Button>Atualizar Pendências</Button>
+      <p className="legend">ÚLTIMA PENDÊNCIA ATUALIZADA EM DD/MM/AAAA HH:DD</p>
+      <Button className="button_normal button-update_pendencies">
+        Atualizar Pendências
+      </Button>
     </div>
   </Container>
 );
--- flaskapp/app/templates/reactapp/src/views/Pacient/Evolution/styles.js
+++ flaskapp/app/templates/reactapp/src/views/Pacient/Evolution/styles.js
@@ -2,4 +2,124 @@ import styled from "styled-components";
 
 export default styled.div`
   margin: 2.625rem 0;
+  width: 100%;
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
+
+  .legend {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+
+  .button_normal {
+    max-width: 11.75rem;
+  }
+
+  .evolution {
+    &_pacient {
+      display: flex;
+      flex-direction: column;
+      row-gap: 1rem;
+
+      .header {
+        display: flex;
+        justify-content: space-between;
+
+        button {
+          max-width: 13.8rem;
+          height: 1.875rem;
+        }
+      }
+
+      .row {
+        display: flex;
+        column-gap: 0.75rem;
+
+        .normal {
+          max-width: 15.5rem;
+        }
+
+        .larger {
+          max-width: 27.5rem;
+        }
+
+        .small {
+          max-width: 12rem;
+        }
+      }
+
+      .legend {
+        margin-top: 0.25rem;
+      }
+    }
+
+    &_button {
+      max-width: 6.125rem;
+      margin-top: -0.5rem;
+      align-self: flex-end;
+    }
+  }
+
+  .prescriptions_pacient {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+
+    h2 {
+      margin-bottom: 1rem;
+    }
+
+    ol {
+      display: flex;
+      flex-direction: column;
+      row-gap: 0.5rem;
+      margin-left: 2rem;
+
+      .error {
+        margin-top: 0.25rem;
+        font-size: 0.625rem;
+        color: ${({ theme }) => theme.colors.red};
+      }
+    }
+
+    .legend {
+      margin-top: 0.5rem;
+    }
+
+    .buttons {
+      display: flex;
+      justify-content: flex-end;
+      column-gap: 0.5rem;
+      margin-top: 0.75rem;
+      margin-bottom: 0.5rem;
+
+      .button_add_prescription {
+        width: 18.25rem;
+      }
+    }
+    .button-update_prescription {
+      align-self: flex-end;
+    }
+  }
+
+  .pendencies_pacient {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+
+    h2 {
+      margin-bottom: 1rem;
+    }
+
+    .legend {
+      margin-top: 0.75rem;
+    }
+
+    .button-update_pendencies {
+      align-self: flex-end;
+      margin-top: 1.5rem;
+    }
+  }
 `;
