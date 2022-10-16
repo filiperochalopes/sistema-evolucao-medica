9307fc36b44e5b7008f3f17c05ee80487378f2f4
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-16 15:24:40 -0300

    feat(helpers): add additionalDataScreen helper

--- flaskapp/app/templates/reactapp/src/components/Modals/components/ModalPrintData/index.jsx
+++ flaskapp/app/templates/reactapp/src/components/Modals/components/ModalPrintData/index.jsx
@@ -1,38 +1,49 @@
+/* eslint-disable import/order */
 import Container from "./styles";
 
 import Button from "components/Button";
+import { useModalContext } from "services/ModalContext";
 import React from "styled-components";
+import additionalDataScreen from "helpers/additionalDataScreen";
 
-const ModalPrintData = () => (
-  <Container>
-    <Button customType="gray" type="button">
-      Ficha de Internamento Hospitalar
-    </Button>
-    <Button customType="gray" type="button">
-      Folha de Admissão do Hospital
-    </Button>
-    <Button customType="gray" type="button">
-      Folha de Evolução
-    </Button>
-    <Button customType="gray" type="button">
-      Folha Prescrição
-    </Button>
-    <Button customType="gray" type="button">
-      Ficha de Alta
-    </Button>
-    <Button customType="gray" type="button">
-      Balanço hídrico
-    </Button>
-    <Button customType="gray" type="button">
-      APAC
-    </Button>
-    <Button customType="gray" type="button">
-      Prescrição Externa
-    </Button>
-    <Button customType="gray" type="button">
-      Solicitação de Exames
-    </Button>
-  </Container>
-);
+const ModalPrintData = ({ confirmButtonAction }) => {
+  const { addModal } = useModalContext();
+
+  return (
+    <Container>
+      <Button customType="gray" type="button">
+        Ficha de Internamento Hospitalar
+      </Button>
+      <Button customType="gray" type="button">
+        Folha de Admissão do Hospital
+      </Button>
+      <Button customType="gray" type="button">
+        Folha de Evolução
+      </Button>
+      <Button customType="gray" type="button">
+        Folha Prescrição
+      </Button>
+      <Button customType="gray" type="button">
+        Ficha de Alta
+      </Button>
+      <Button customType="gray" type="button">
+        Balanço hídrico
+      </Button>
+      <Button
+        onClick={() => addModal(additionalDataScreen)}
+        customType="gray"
+        type="button"
+      >
+        APAC
+      </Button>
+      <Button customType="gray" type="button">
+        Prescrição Externa
+      </Button>
+      <Button customType="gray" type="button">
+        Solicitação de Exames
+      </Button>
+    </Container>
+  );
+};
 
 export default ModalPrintData;
---
+++ flaskapp/app/templates/reactapp/src/helpers/additionalDataScreen.js
@@ -0,0 +1,8 @@
+const additionalDataScreen = {
+  confirmButtonAction: () => {},
+  content: <></>,
+  returnButtonAction: () => {},
+  title: "Preencha os campos adicionais",
+};
+
+export default additionalDataScreen;
