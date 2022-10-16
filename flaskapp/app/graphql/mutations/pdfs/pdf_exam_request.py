1032194f0ed50f17b1fa97d7e285da70460a499f
ckk3 <contato.gustavomota@gmil.com>
2022-10-15 23:32:30 -0300

    add exam and breaklines working

--- flaskapp/app/graphql/mutations/pdfs/pdf_exam_request.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_exam_request.py
@@ -6,6 +6,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from flask import Response
+from math import ceil
 
 # Doing the import this way only when is called by antoher file (like pytest)
 if __name__ != "__main__":
@@ -16,12 +17,15 @@ for x in range(0, 2000):
     lenghtTest += str(x)
 
 #The templat will change depending on exems lenght
-pags_quant = 1
+global pags_quant
+pags_quant = 0
 template_directory = ["./graphql/mutations/pdfs/pdfs_templates/one_exam_request.pdf", "./graphql/mutations/pdfs/pdfs_templates/two_exam_request.pdf", "./graphql/mutations/pdfs/pdfs_templates/three_exam_request.pdf"]
 font_directory = "./graphql/mutations/pdfs/Roboto-Mono.ttf"
 
 
-def fill_pdf_exam_request(patient_name:str, patient_cns:int, patient_birthday:datetime.datetime, patient_adress:str, solicitation_reason:str, prof_solicitor:str, prof_authorized:str, solicitation_datetime:datetime.datetime, autorization_datetime:datetime.datetime=None, document_pacient_date:datetime.datetime=None, document_pacient_name:str=None):
+def fill_pdf_exam_request(patient_name:str, patient_cns:int, patient_birthday:datetime.datetime, patient_adress:str, solicitation_reason:str,
+exams:str, prof_solicitor:str, prof_authorized:str, solicitation_datetime:datetime.datetime, autorization_datetime:datetime.datetime=None, document_pacient_date:datetime.datetime=None, document_pacient_name:str=None):
+
     try:
         packet = io.BytesIO()
         # Create canvas and add data
@@ -30,10 +34,13 @@ def fill_pdf_exam_request(patient_name:str, patient_cns:int, patient_birthday:da
         # this is also changed in the document to some especific fields
         pdfmetrics.registerFont(TTFont('Roboto-Mono', font_directory))
         c.setFont('Roboto-Mono', 9)
-    
+
         # Writing all data in respective fields
         # not null data
         try:
+            c = add_exams(canvas=c, exams=exams)
+            # verify if c is a error at some point
+            if type(c) == type(Response()): return c
             c = add_patientName(canvas=c, name=patient_name)
             # verify if c is a error at some point
             if type(c) == type(Response()): return c
@@ -44,7 +51,7 @@ def fill_pdf_exam_request(patient_name:str, patient_cns:int, patient_birthday:da
             else:
                 return Response('Some error happen when adding not null data to fields', status=500)
 
-    # create a new PDF with Reportlab
+        # create a new PDF with Reportlab
         c.save()
         packet.seek(0)
         new_pdf = PdfReader(packet)
@@ -85,6 +92,57 @@ def add_patientName(canvas:canvas.Canvas, name:str):
         return Response('Unknow error while adding patient name', status=500)
 
 
+def add_exams(canvas:canvas.Canvas, exams:str):
+    """add solicited exams
+
+    Args:
+        canvas (canvas.Canvas): canvas to use
+        exams (str): exams
+
+    Returns:
+        canvas or Response:canvas if everthing is allright or Response if hapens some error 
+    """    
+    try:
+        if type(exams) != type(str()):
+            return Response('Exams has to be a string', status=400)
+        exams = exams.strip()
+        if len(exams) > 972 or len(exams) < 5:
+            return Response('Exams has to be at least 5 characters and no more than 972 characters', status=400)
+        # Making the line break whem has 105 charater in a line
+        str_exams = ''
+        exams = lenghtTest.strip()
+        #Calculate how many pags will have, ceil function round to upper int
+        pags_quant = ceil(len(exams)/324)
+        charByLine = 108
+        brokeLinexTimes = int(len(exams)/charByLine)
+        currentLine = charByLine
+        lastline = 0
+        yposition = 649
+        cont = 0
+        for x in range(pags_quant):
+            while brokeLinexTimes >= 0:
+                str_exams = exams[lastline:currentLine]
+                canvas = global_functions.add_data(canvas=canvas, data=str_exams, pos=(7, yposition))
+                lastline = currentLine
+                currentLine += charByLine
+                brokeLinexTimes -= 1
+                cont += 1
+                if cont%3 == 0:
+                    break
+                yposition -= 10
+            yposition -= 260
+
+        del(str_exams)
+        del(brokeLinexTimes)
+        del(currentLine)
+        del(lastline)
+        del(yposition)
+        return canvas
+    except:
+        return Response('Unknow error while adding Solicited Exams', status=500)
+
+
+
 
 
 if __name__ == "__main__":
@@ -94,6 +152,7 @@ if __name__ == "__main__":
         patient_cns=928976954930007, 
         patient_birthday=datetime.datetime.now(), 
         patient_adress="Patient Adress", 
+        exams='amniocentesis, blood analysis, gastric fluid analysis, kidney function test, liver function test, lumbar puncture, malabsorption test,Pap smear.',
         solicitation_reason="Solicitation Reason", 
         prof_solicitor="Professional Solicitor", 
         prof_authorized="Professional Authorized", 
