bbc5cd96a1b16d20d272c8d145f5b0dfbf464db4
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-17 11:25:55 -0300

    feat(view): add formik in form of login view

---
+++ flaskapp/app/templates/reactapp/src/schemas/loginSchema.js
@@ -0,0 +1,6 @@
+import * as Yup from "yup";
+
+export default Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+});
--- flaskapp/app/templates/reactapp/src/views/Login/index.jsx
+++ flaskapp/app/templates/reactapp/src/views/Login/index.jsx
@@ -3,23 +3,40 @@ import Container from "./styles";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 
+import { useFormik } from "formik";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import loginSchema from "schemas/loginSchema";
 
 function Login() {
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      navigate("/pacientes");
+    },
+  });
   return (
-    <Container>
+    <Container onSubmit={formik.handleSubmit}>
       <h2>Realizar Login</h2>
-      <Input placeholder="Login" />
-      <Input placeholder="Senha" />
-      <Button
-        onClick={() => {
-          navigate("/pacientes");
-        }}
-      >
-        Entrar
-      </Button>
+      <Input
+        placeholder="Login"
+        name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+      />
+      <Input
+        placeholder="Senha"
+        name="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+      />
+      <Button>Entrar</Button>
     </Container>
   );
 }
