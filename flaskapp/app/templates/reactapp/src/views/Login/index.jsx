7ff53cd9735c42b479fa0e029f2e7293b279b010
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-17 11:11:05 -0300

    feat(view): add form element in login view

--- flaskapp/app/templates/reactapp/src/views/Login/index.jsx
+++ flaskapp/app/templates/reactapp/src/views/Login/index.jsx
@@ -4,14 +4,22 @@ import Button from "../../components/Button";
 import Input from "../../components/Input";
 
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <Container>
       <h2>Realizar Login</h2>
       <Input placeholder="Login" />
       <Input placeholder="Senha" />
-      <Button>Entrar</Button>
+      <Button
+        onClick={() => {
+          navigate("/pacientes");
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 }
