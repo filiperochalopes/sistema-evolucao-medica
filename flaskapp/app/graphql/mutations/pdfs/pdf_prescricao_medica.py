fc6b99df264d6f8a3165c4132b15e6551509f605
ckk3 <contato.gustavomota@gmil.com>
2022-10-21 20:19:08 -0300

    changing pagesizePoints to page_size_points

--- flaskapp/app/graphql/mutations/pdfs/pdf_prescricao_medica.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_prescricao_medica.py
@@ -17,7 +17,7 @@ for x in range(0, 2000):
     lenghtTest += str(x)
 
 template_directory = "/app/app/assets/pdfs_templates/two_pages_precricao_medica_template.pdf"
-pagesizePoints = (841.92, 595.2)
+page_size_points = (841.92, 595.2)
 font_directory = "/app/app/assets/pdfs_templates/Roboto-Mono.ttf"
 
 def fill_pdf_prescricao_medica(document_datetime:datetime.datetime, patient_name:str, prescription:list) -> Union[PdfWriter, Response]:
@@ -26,7 +26,7 @@ def fill_pdf_prescricao_medica(document_datetime:datetime.datetime, patient_name
         try:
             packet = io.BytesIO()
             # Create canvas and add data
-            c = canvas.Canvas(packet, pagesize=pagesizePoints)
+            c = canvas.Canvas(packet, pagesize=page_size_points)
             # Change canvas font to mach with the document
             # this is also changed in the document to some especific fields
             pdfmetrics.registerFont(TTFont('Roboto-Mono', font_directory))
