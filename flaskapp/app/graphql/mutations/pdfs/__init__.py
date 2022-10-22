af7d7179d5695576e220d5108b83bbc927624ae0
ckk3 <contato.gustavomota@gmil.com>
2022-10-21 22:53:36 -0300

    adding all in apac

--- flaskapp/app/graphql/mutations/pdfs/__init__.py
+++ flaskapp/app/graphql/mutations/pdfs/__init__.py
@@ -1,3 +1,8 @@
 from . import global_functions
 from . import pdf_aih_sus
+from . import pdf_apac
+from . import pdf_exam_request
+from . import pdf_ficha_internamento
+from . import pdf_prescricao_medica
+from . import pdf_relatorio_de_alta
 
