1b447cc0dc80d76f4b757fe52f8bdaae88f1cb26
ckk3 <contato.gustavomota@gmil.com>
2022-10-28 00:41:26 -0300

    create 2 pages mix

--- flaskapp/app/graphql/mutations/pdfs/pdf_solicit_mamografia.py
+++ flaskapp/app/graphql/mutations/pdfs/pdf_solicit_mamografia.py
@@ -16,19 +16,18 @@ def fill_pdf_solicit_mamografia(patient_name:str, patient_cns:int, patient_mothe
     try:
         packet = io.BytesIO()
         # Create canvas and add data
-        #####c = canvas.Canvas(packet, pagesize=letter)
+        c = canvas.Canvas(packet, pagesize=letter)
         packet_2 = io.BytesIO()
         # Create canvas and add data
         c_2 = canvas.Canvas(packet_2, pagesize=letter)
         # Change canvas font to mach with the document
         # this is also changed in the document to some especific fields
         pdfmetrics.registerFont(TTFont('Roboto-Mono', FONT_DIRECTORY))
-        #####c.setFont('Roboto-Mono', 13)
+        c.setFont('Roboto-Mono', 13)
         c_2.setFont('Roboto-Mono', 13)
         # Writing all data in respective fields
         # not null data
 
-        old = """
         try:
             c = pdf_functions.add_cns(can=c, cns=patient_cns, pos=(46, 676), camp_name='Patient CNS', interval=' ')
             if type(c) == type(Response()): return c
@@ -125,7 +124,7 @@ def fill_pdf_solicit_mamografia(patient_name:str, patient_cns:int, patient_mothe
         except:
             return Response('Critical error happen when adding data that can be null to fields in page 1', status=500)
 
-"""
+
 ### Add Page 2
         packet_2 = io.BytesIO()
         # Create canvas and add data
@@ -161,21 +160,21 @@ def fill_pdf_solicit_mamografia(patient_name:str, patient_cns:int, patient_mothe
                 return Response('Some error happen when adding not null data to fields in page 2', status=500)
 
         # create a new PDF with Reportlab
-        ######c.save()
+        c.save()
         c_2.save()
-        ######packet.seek(0)
+        packet.seek(0)
         packet_2.seek(0)
-        ######new_pdf = PdfReader(packet)
+        new_pdf = PdfReader(packet)
         new_pdf_2 = PdfReader(packet_2)
         # read the template pdf 
         template_pdf = PdfReader(open(TEMPLATE_SOLICIT_MAMOGRAFIA_DIRECTORY, "rb"))
         output = PdfWriter()
         # add the "watermark" (which is the new pdf) on the existing page
-        ######page = template_pdf.pages[0]
-        ######page.merge_page(new_pdf.pages[0])
+        page = template_pdf.pages[0]
+        page.merge_page(new_pdf.pages[0])
         page_2 = template_pdf.pages[1]
         page_2.merge_page(new_pdf_2.pages[0])
-        ######output.add_page(page)
+        output.add_page(page)
         output.add_page(page_2)
 
         pdf_functions.write_newpdf(output, WRITE_SOLICIT_MAMOGRAFIA_DIRECTORY)
