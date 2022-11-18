fd5f42eebb013b1fe266b31cdf7942cd67752c1f
ckk3 <contato.gustavomota@gmil.com>
2022-11-18 18:15:31 -0300

    Added template base url and write base url

--- flaskapp/app/env.py
+++ flaskapp/app/env.py
@@ -12,24 +12,26 @@ FONT_DIRECTORY = "/app/app/assets/pdfs_templates/Roboto-Mono.ttf"
 GRAPHQL_MUTATION_QUERY_URL = "http://localhost:5000/api/v1/graphql"
 
 # Templates Directorys
-TEMPLATE_AIH_SUS_DIRECTORY = "/app/app/assets/pdfs_templates/aih_sus.pdf"
-TEMPLATE_APAC_DIRECTORY = "/app/app/assets/pdfs_templates/apac.pdf"
-TEMPLATE_EXAM_REQUEST_DIRECTORY = ["/app/app/assets/pdfs_templates/one_exam_request.pdf", "/app/app/assets/pdfs_templates/two_exam_request.pdf", "/app/app/assets/pdfs_templates/three_exam_request.pdf"]
-TEMPLATE_FICHA_INTERN_DIRECTORY = "/app/app/assets/pdfs_templates/ficha_de_internamento_hmlem.pdf"
-TEMPLATE_LME_DIRECTORY = "/app/app/assets/pdfs_templates/lme.pdf"
-TEMPLATE_PRESCRICAO_MEDICA_DIRECTORY = "/app/app/assets/pdfs_templates/two_pages_precricao_medica_template.pdf"
-TEMPLATE_RELATORIO_ALTA_DIRECTORY = "/app/app/assets/pdfs_templates/relatorio_de_alta.pdf"
-TEMPLATE_SOLICIT_MAMOGRAFIA_DIRECTORY = "/app/app/assets/pdfs_templates/solicitacao_mamografia.pdf"
+TEMPLATE_BASE_URL = "/app/app/assets/pdfs_templates/"
+TEMPLATE_AIH_SUS_DIRECTORY              = f"{TEMPLATE_BASE_URL}aih_sus.pdf"
+TEMPLATE_APAC_DIRECTORY                 = f"{TEMPLATE_BASE_URL}apac.pdf"
+TEMPLATE_EXAM_REQUEST_DIRECTORY         = [f"{TEMPLATE_BASE_URL}one_exam_request.pdf", f"{TEMPLATE_BASE_URL}two_exam_request.pdf", f"{TEMPLATE_BASE_URL}three_exam_request.pdf"]
+TEMPLATE_FICHA_INTERN_DIRECTORY         = f"{TEMPLATE_BASE_URL}ficha_de_internamento_hmlem.pdf"
+TEMPLATE_LME_DIRECTORY                  = f"{TEMPLATE_BASE_URL}lme.pdf"
+TEMPLATE_PRESCRICAO_MEDICA_DIRECTORY    = f"{TEMPLATE_BASE_URL}two_pages_precricao_medica_template.pdf"
+TEMPLATE_RELATORIO_ALTA_DIRECTORY       = f"{TEMPLATE_BASE_URL}relatorio_de_alta.pdf"
+TEMPLATE_SOLICIT_MAMOGRAFIA_DIRECTORY   = f"{TEMPLATE_BASE_URL}solicitacao_mamografia.pdf"
 
-# Write directories when creating pdf that will be enconded to base64 
-WRITE_AIH_SUS_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/aih_sus_teste.pdf"
-WRITE_APAC_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/apac_teste.pdf"
-WRITE_EXAM_REQUEST_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/exam_request_teste.pdf"
-WRITE_FICHA_INTERN_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/ficha_teste.pdf"
-WRITE_LME_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/lme_teste.pdf"
-WRITE_PRESCRICAO_MEDICA_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/prescricao_medica_teste.pdf"
-WRITE_RELATORIO_ALTA_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/relatorio_alta_teste.pdf"
-WRITE_SOLICIT_MAMOGRAFIA_DIRECTORY = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/solicit_mamografia_teste.pdf"
+# Write directories when creating pdf that will be enconded to base64
+WRITE_BASE_URL = "/app/app/graphql/mutations/pdfs/tests/pdfs_created_files_test/"
+WRITE_AIH_SUS_DIRECTORY             = f"{WRITE_BASE_URL}aih_sus_teste.pdf"
+WRITE_APAC_DIRECTORY                = f"{WRITE_BASE_URL}apac_teste.pdf"
+WRITE_EXAM_REQUEST_DIRECTORY        = f"{WRITE_BASE_URL}exam_request_teste.pdf"
+WRITE_FICHA_INTERN_DIRECTORY        = f"{WRITE_BASE_URL}ficha_teste.pdf"
+WRITE_LME_DIRECTORY                 = f"{WRITE_BASE_URL}lme_teste.pdf"
+WRITE_PRESCRICAO_MEDICA_DIRECTORY   = f"{WRITE_BASE_URL}prescricao_medica_teste.pdf"
+WRITE_RELATORIO_ALTA_DIRECTORY      = f"{WRITE_BASE_URL}relatorio_alta_teste.pdf"
+WRITE_SOLICIT_MAMOGRAFIA_DIRECTORY  = f"{WRITE_BASE_URL}solicit_mamografia_teste.pdf"
 
 class InstitutionData:
     # Dados iniciais da instituição para cadastro do mesmo pelo `flask seed` para preenchimento de dados iniciais
