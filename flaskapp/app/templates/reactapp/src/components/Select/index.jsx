fa459f4cb84baeda0fafac5bca2bc97c79b9780a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-11-20 17:28:33 -0300

    feat(component): add created component in select

--- flaskapp/app/templates/reactapp/src/components/Select/index.jsx
+++ flaskapp/app/templates/reactapp/src/components/Select/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactSelect from "react-select";
+import Creatable from "react-select/creatable";
 import Container, { IconContainer } from "./styles";
 import { MdArrowDropDown } from "react-icons/md";
 
@@ -24,20 +25,25 @@ const selectStyles = {
   }),
 };
 
-const Select = ({ className, ...rest }) => (
-  <Container className={className}>
-    <ReactSelect
-      components={{
-        DropdownIndicator: () => (
-          <IconContainer>
-            <MdArrowDropDown size={24} />
-          </IconContainer>
-        ),
-      }}
-      styles={selectStyles}
-      {...rest}
-    />
-  </Container>
-);
+const Select = ({ className, created = false, components = {}, ...rest }) => {
+  const SelectType = created ? Creatable : ReactSelect;
+
+  return (
+    <Container className={className}>
+      <SelectType
+        components={{
+          DropdownIndicator: () => (
+            <IconContainer>
+              <MdArrowDropDown size={24} />
+            </IconContainer>
+          ),
+          ...components,
+        }}
+        styles={selectStyles}
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Select;
