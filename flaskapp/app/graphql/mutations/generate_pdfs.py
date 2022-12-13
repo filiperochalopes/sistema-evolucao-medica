bf50b7ad3ed0cff4cb5513d276fab1112e42193a
Ckk3 <contato.gustavomota@gmail.com>
2022-12-13 12:26:37 -0300

    update pdf apac to new directory folders

--- flaskapp/app/graphql/mutations/generate_pdfs.py
+++ flaskapp/app/graphql/mutations/generate_pdfs.py
@@ -5,9 +5,15 @@ import sys
 from ariadne import convert_kwargs_to_snake_case
 from app.graphql import mutation
 from app.services.functions.pdfs.func_generate_pdf_aih_sus import func_generate_pdf_aih_sus
+from app.services.functions.pdfs.func_generate_pdf_apac import func_generate_pdf_apac
 
 
 @mutation.field('generatePdf_AihSus')
 @convert_kwargs_to_snake_case
 def generate_pdf_aih_sus(_, info, **kwargs):
     return func_generate_pdf_aih_sus(**kwargs)
+
+@mutation.field('generatePdf_Apac')
+@convert_kwargs_to_snake_case
+def generate_pdf_apac(_, info, **kwargs):
+    return func_generate_pdf_apac(**kwargs)
