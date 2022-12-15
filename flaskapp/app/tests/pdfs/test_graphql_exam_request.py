1407975cde8ef35f7931bc958820aac0f1abbd9a
Ckk3 <contato.gustavomota@gmail.com>
2022-12-15 12:56:07 -0300

    update exam requests tests to parametrize

--- flaskapp/app/tests/pdfs/test_graphql_exam_request.py
+++ flaskapp/app/tests/pdfs/test_graphql_exam_request.py
@@ -2,6 +2,7 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 import datetime
 from app.env import GRAPHQL_MUTATION_QUERY_URL
+import pytest
 
 global lenght_test
 lenght_test = ''
@@ -101,17 +102,14 @@ def test_awnser_with_only_required_data():
 # short name
 # wrong name type
 
-def test_empty_prof_authorized():    
-    assert data_to_use(prof_authorized_name='') == True
 
-def test_with_space_prof_authorized():    
-    assert data_to_use(prof_authorized_name='  ') == True
+@pytest.mark.parametrize("test_input", ['    ', ''])
+def test_empty_value_prof_authorized_name(test_input):
+    assert data_to_use(prof_authorized_name=test_input) == True
 
-def test_empty_document_pacient_name():    
-    assert data_to_use(document_pacient_name='') == True
-
-def test_with_space_document_pacient_name():    
-    assert data_to_use(document_pacient_name='  ') == True
+@pytest.mark.parametrize("test_input", ['    ', ''])
+def test_empty_value_document_pacient_name(test_input):
+    assert data_to_use(document_pacient_name=test_input) == True
 
 
 #################################################################
