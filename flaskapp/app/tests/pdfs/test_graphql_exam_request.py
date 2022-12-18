78aa4426e56f31112cd21c1805c511282ef29a6b
Ckk3 <contato.gustavomota@gmail.com>
2022-12-18 16:44:27 -0300

    adding fixtures to exam request tests

--- flaskapp/app/tests/pdfs/test_graphql_exam_request.py
+++ flaskapp/app/tests/pdfs/test_graphql_exam_request.py
@@ -4,20 +4,37 @@ import datetime
 from app.env import GRAPHQL_MUTATION_QUERY_URL
 import pytest
 
-global lenght_test
-lenght_test = ''
-for x in range(0, 1100):
-    lenght_test += str(x)
+@pytest.fixture
+def lenght_test():
+    """generate a string with data with charactes to test lenght"""
+    lenght_test = ''
+    for x in range(0, 1100):
+        lenght_test += str(x)
+    return lenght_test
 
-datetime_to_use = datetime.datetime.now().strftime('%d/%m/%Y')
+@pytest.fixture
+def datetime_to_use():
+    """get current datetime to test"""
+    return datetime.datetime.now().strftime('%d/%m/%Y')
 
-# Select your transport with ag graphql url endpoint
-transport = AIOHTTPTransport(url=GRAPHQL_MUTATION_QUERY_URL)
+@pytest.fixture
+def client():
+    # Select your transport with ag graphql url endpoint
+    transport = AIOHTTPTransport(url=GRAPHQL_MUTATION_QUERY_URL)
+    # Create a GraphQL client using the defined transport
+    return Client(transport=transport, fetch_schema_from_transport=True)
 
-# Create a GraphQL client using the defined transport
-client = Client(transport=transport, fetch_schema_from_transport=True)
+def data_to_use(client, datetime_to_use, patient_name='Patient Name',patient_cns='928976954930007', patient_birthday=None,patient_adress="Patient Adress",exams="Exames tests with a text",solicitation_reason="Solicitation Reason",prof_solicitor_name="Professional Solicitor",prof_authorized_name="Professional Authorized",solicitation_datetime=None,autorization_datetime=None, document_pacient_date=None,document_pacient_name='Document pacient name'):
 
-def data_to_use(patient_name='Patient Name',patient_cns='928976954930007', patient_birthday=datetime_to_use,patient_adress="Patient Adress",exams=lenght_test[:800],solicitation_reason="Solicitation Reason",prof_solicitor_name="Professional Solicitor",prof_authorized_name="Professional Authorized",solicitation_datetime=datetime_to_use,autorization_datetime=datetime_to_use, document_pacient_date=datetime_to_use,document_pacient_name='Document pacient name'):
+    if patient_birthday == None:
+        patient_birthday = datetime_to_use
+    if solicitation_datetime == None:
+        solicitation_datetime = datetime_to_use
+    if document_pacient_date == None:
+        document_pacient_date = datetime_to_use
+    if autorization_datetime == None:
+        autorization_datetime = datetime_to_use
+
     request_string = """
         mutation{
             generatePdf_SolicitExames("""
@@ -51,13 +68,13 @@ def data_to_use(patient_name='Patient Name',patient_cns='928976954930007', patie
         return False 
 
 #Testing Ficha Internamento
-def test_with_data_in_function():
-    assert data_to_use() == True
+def test_with_data_in_function(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use) == True
 
-def test_answer_with_all_fields():
-    assert data_to_use() == True
+def test_answer_with_all_fields(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use) == True
 
-def test_awnser_with_only_required_data():
+def test_awnser_with_only_required_data(client, datetime_to_use):
     request_string = """
         mutation{
             generatePdf_SolicitExames("""
@@ -104,12 +121,12 @@ def test_awnser_with_only_required_data():
 
 
 @pytest.mark.parametrize("test_input", ['    ', ''])
-def test_empty_value_prof_authorized_name(test_input):
-    assert data_to_use(prof_authorized_name=test_input) == True
+def test_empty_value_prof_authorized_name(test_input, client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use, prof_authorized_name=test_input) == True
 
 @pytest.mark.parametrize("test_input", ['    ', ''])
-def test_empty_value_document_pacient_name(test_input):
-    assert data_to_use(document_pacient_name=test_input) == True
+def test_empty_value_document_pacient_name(test_input, client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use, document_pacient_name=test_input) == True
 
 
 #################################################################
@@ -121,17 +138,17 @@ def test_empty_value_document_pacient_name(test_input):
 # test wrong type
 # test valid datetime
 
-def test_valid_patient_birthday():
-    assert data_to_use(patient_birthday=datetime_to_use) == True
+def test_valid_patient_birthday(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use,patient_birthday=datetime_to_use) == True
 
-def test_valid_solicitation_datetime():
-    assert data_to_use(solicitation_datetime=datetime_to_use) == True
+def test_valid_solicitation_datetime(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use,solicitation_datetime=datetime_to_use) == True
 
-def test_valid_autorization_datetime():
-    assert data_to_use(autorization_datetime=datetime_to_use) == True
+def test_valid_autorization_datetime(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use,autorization_datetime=datetime_to_use) == True
 
-def test_valid_document_pacient_date():
-    assert data_to_use(document_pacient_date=datetime_to_use) == True
+def test_valid_document_pacient_date(client, datetime_to_use):
+    assert data_to_use(client, datetime_to_use,document_pacient_date=datetime_to_use) == True
 
 
 #############################################################################
@@ -146,14 +163,17 @@ def test_valid_document_pacient_date():
 # test 2 pages long
 # test 3 pages long
 
-def test_1_pages_long_exams():
-    assert data_to_use(exams=lenght_test[:200]) == True
+def test_1_pages_long_exams(client, datetime_to_use, lenght_test):
+    text = lenght_test[:200]
+    assert data_to_use(client, datetime_to_use,exams=text) == True
 
-def test_2_pages_long_exams():
-    assert data_to_use(exams=lenght_test[:400]) == True
+def test_2_pages_long_exams(client, datetime_to_use, lenght_test):
+    text = lenght_test[:400]
+    assert data_to_use(client, datetime_to_use,exams=text) == True
 
-def test_3_pages_long_exams():
-    assert data_to_use(exams=lenght_test[:750]) == True
+def test_3_pages_long_exams(client, datetime_to_use, lenght_test):
+    text = lenght_test[:750]
+    assert data_to_use(client, datetime_to_use,exams=text) == True
 
 
 
