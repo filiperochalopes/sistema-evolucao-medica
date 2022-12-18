d75fcbf9379c9fc5238fe71e2095025b2b9d6b1f
Ckk3 <contato.gustavomota@gmail.com>
2022-12-18 17:09:43 -0300

    create test to 2 pages exam request

--- flaskapp/app/tests/pdfs/test_create_pdfs.py
+++ flaskapp/app/tests/pdfs/test_create_pdfs.py
@@ -1,9 +1,9 @@
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from base64 import b64decode
-from app.env import GRAPHQL_MUTATION_QUERY_URL, WRITE_DECODE_BASE_URL, WRITE_DECODE_AIH_SUS_DIRECTORY, WRITE_DECODE_APAC_DIRECTORY,WRITE_DECODE_EXAM_REQUEST_DIRECTORY,WRITE_DECODE_FICHA_INTERN_DIRECTORY, WRITE_DECODE_LME_DIRECTORY, WRITE_DECODE_PRESCRICAO_MEDICA_DIRECTORY, WRITE_DECODE_RELATORIO_ALTA_DIRECTORY, WRITE_DECODE_SOLICIT_MAMOGRAFIA_DIRECTORY
+from app.env import GRAPHQL_MUTATION_QUERY_URL, WRITE_DECODE_BASE_URL, WRITE_DECODE_AIH_SUS_DIRECTORY, WRITE_DECODE_APAC_DIRECTORY,WRITE_DECODE_EXAM_REQUEST_DIRECTORY,WRITE_DECODE_FICHA_INTERN_DIRECTORY, WRITE_DECODE_LME_DIRECTORY, WRITE_DECODE_PRESCRICAO_MEDICA_DIRECTORY, WRITE_DECODE_RELATORIO_ALTA_DIRECTORY, WRITE_DECODE_SOLICIT_MAMOGRAFIA_DIRECTORY, WRITE_DECODE_EXAM_REQUEST_2_PAGES_DIRECTORY
 import pytest
-from app.tests.pdfs.request_queries_examples import aih_sus_request_string, apac_request_string, exam_request_request_string, ficha_internamento_request_string, prescricao_medica_request_string, lme_request_string, solicit_mamografia_request_string, relatorio_alta_request_string
+from app.tests.pdfs.request_queries_examples import aih_sus_request_string, apac_request_string, exam_request_request_string, ficha_internamento_request_string, prescricao_medica_request_string, lme_request_string, solicit_mamografia_request_string, relatorio_alta_request_string, exam_request_2_pages_request_string
 
 @pytest.fixture
 def client():
@@ -105,6 +105,35 @@ def test_decode_base64_exam_request_pdf(client):
 
     assert created == True
 
+def test_create_exam_request_2_pages_pdf(client):
+    query = gql(exam_request_2_pages_request_string)
+    result = False
+    try:
+        #When some exception is created in grphql he return a error
+        client.execute(query)
+        result = True
+    except:
+        result = False 
+    
+    assert result == True
+
+def test_decode_base64_exam_request_2_pages_pdf(client):
+    query = gql(exam_request_2_pages_request_string)
+    created = False
+    try:
+        #When some exception is created in grphql he return a error
+        result = client.execute(query)
+        generated_pdf_b64 = b64decode(result['generatePdf_SolicitExames']['base64Pdf'], validate=True)
+
+
+        f = open(WRITE_DECODE_EXAM_REQUEST_2_PAGES_DIRECTORY, 'wb')
+        f.write(generated_pdf_b64)
+        f.close()
+        created = True
+    except:
+        created = False
+
+    assert created == True
 
 def test_create_ficha_internamento_pdf(client):
     query = gql(ficha_internamento_request_string)
