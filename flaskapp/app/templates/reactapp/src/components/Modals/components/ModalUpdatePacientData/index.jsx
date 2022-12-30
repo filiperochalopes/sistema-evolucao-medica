abaf261f014f1698a81160e5859b5f22a8248b06
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-12-30 20:53:44 -0300

    feat(components): integrate ModalUpdatePacientData

--- flaskapp/app/templates/reactapp/src/components/Modals/components/ModalUpdatePacientData/index.jsx
+++ flaskapp/app/templates/reactapp/src/components/Modals/components/ModalUpdatePacientData/index.jsx
@@ -4,25 +4,31 @@ import Button from "components/Button";
 import Input from "components/Input";
 import React from "styled-components";
 import { useFormik } from "formik";
-import { useQuery } from "@apollo/client";
-import { ALLERGIES, COMORBIDITIES } from "graphql/queries";
+import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import { ALLERGIES, COMORBIDITIES, GET_PATIENT, STATES } from "graphql/queries";
 import { useEffect } from "react";
 import getCepApiAdapter from "services/getCepApiAdapter";
 import Select from "components/Select";
+import { UPDATE_PATIENT } from "graphql/mutations";
 
-const ModalUpdatePacientData = () => {
+const ModalUpdatePacientData = ({ id }) => {
   const { data: allergiesData } = useQuery(ALLERGIES);
   const { data: comorbiditiesData } = useQuery(COMORBIDITIES);
+  const [getPatientData] = useLazyQuery(GET_PATIENT);
+  const [updatePatient] = useMutation(UPDATE_PATIENT);
+  const { data: statesData } = useQuery(STATES);
+
   const formik = useFormik({
     initialValues: {
       name: "",
       sex: "",
-      birthday: "",
+      birthdate: "",
       cpf: "",
       cns: "",
       rg: "",
-      commorbidities: [],
+      comorbidities: [],
       allergies: [],
+      weightKg: "",
       address: {
         zipcode: "",
         street: "dd",
@@ -32,31 +38,57 @@ const ModalUpdatePacientData = () => {
         uf: "",
       },
     },
+    onSubmit: (values) => {
+      updatePatient({
+        variables: {
+          id: id,
+          patient: {
+            ...values,
+            sex: values.sex.value,
+            comorbidities: values.comorbidities.map(
+              (commorbiditie) => commorbiditie.id
+            ),
+            allergies: values.allergies.map((allergie) => allergie.id),
+            weightKg: parseFloat(values.weightKg),
+            address: {
+              ...values.address,
+              uf: values.address.uf.uf,
+            },
+          },
+        },
+      });
+    },
   });
+  useEffect(() => {
+    getPatientData({
+      variables: {
+        id: id,
+      },
+    });
+  }, []);
 
   useEffect(() => {
     async function getCep() {
       try {
-        const response = await getCepApiAdapter(
-          formik.values.patient.address.zipcode
+        const response = await getCepApiAdapter(formik.values.address.zipcode);
+        const findUf = statesData.state.find(
+          (state) => state.uf === response.data.uf
         );
         formik.setValues({
           ...formik.values,
-          patient: {
-            ...formik.values.patient,
-            address: response.data,
-          },
+          address: { ...response.data, uf: findUf },
         });
+
         // eslint-disable-next-line no-empty
       } catch (e) {
         console.log(e.message);
       }
     }
     getCep();
-  }, [formik.values.patient.address.zipcode]);
+  }, [formik.values.address.zipcode]);
 
   return (
-    <Container>
+    <Container onSubmit={formik.handleSubmit}>
       <div className="row">
         <Input
           className="larger"
@@ -69,13 +101,19 @@ const ModalUpdatePacientData = () => {
           placeholder="Data de Nascimento"
           onChange={formik.handleChange}
           type="date"
-          name="birthday"
+          name="birthdate"
         />
-        <Input
-          className="small"
+        <Select
+          onChange={(e) => {
+            formik.setFieldValue("sex", e);
+          }}
+          value={formik.values.sex}
           placeholder="Sexo"
-          onChange={formik.handleChange}
-          name="sex"
+          className="small"
+          options={[
+            { label: "Masculino", value: "male" },
+            { label: "Feminino", value: "fema" },
+          ]}
         />
       </div>
       <div className="row">
@@ -91,17 +129,40 @@ const ModalUpdatePacientData = () => {
           onChange={formik.handleChange}
           name="address.city"
         />
-        <Input
+
+        <Select
+          onChange={(e) => {
+            formik.setFieldValue("address.uf", e);
+          }}
           className="small"
+          getOptionLabel={(option) => option.name}
+          getOptionValue={(option) => option.uf}
+          value={formik.values.address.uf}
           placeholder="Estado"
-          onChange={formik.handleChange}
-          name="address.uf"
+          options={statesData?.state || []}
         />
         <Input
           className="small"
           placeholder="CEP"
           onChange={formik.handleChange}
           name="address.zipcode"
+        />
+      </div>
+      <div className="row">
+        <Input
+          onChange={formik.handleChange}
+          className="small"
+          name="weightKg"
+          value={formik.values.weightKg}
+          placeholder="Peso"
+        />
+
+        <Input
+          onChange={formik.handleChange}
+          className="small"
+          name="rg"
+          value={formik.values.rg}
+          placeholder="RG"
         />
       </div>
       <div className="row">
@@ -131,9 +192,9 @@ const ModalUpdatePacientData = () => {
       />
       <Select
         onChange={(e) => {
-          formik.setFieldValue("commorbidities", e);
+          formik.setFieldValue("comorbidities", e);
         }}
-        value={formik.values.commorbidities}
+        value={formik.values.comorbidities}
         placeholder="COMORBIDADES"
         getOptionLabel={(option) => option.value}
         getOptionValue={(option) => option.id}
