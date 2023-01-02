2ea5cff7831f04c1ae29d3a24565dad0ebe1daa1
Ckk3 <contato.gustavomota@gmail.com>
2023-01-01 22:36:01 -0300

    adding datetimes data to folha de prescricao

--- flaskapp/app/services/functions/pdfs/func_generate_pdf_folha_prescricao.py
+++ flaskapp/app/services/functions/pdfs/func_generate_pdf_folha_prescricao.py
@@ -1,4 +1,5 @@
 from app.services.utils.PdfFolhaPrescricao import PdfFolhaPrescricao
+import datetime
 
 
 def func_generate_pdf_folha_prescricao(timestamp_start:str, timestamp_ending:str, prescriptions:list) -> str:
@@ -23,13 +24,20 @@ def func_generate_pdf_folha_prescricao(timestamp_start:str, timestamp_ending:str
         for x in range(0, 1100):
             lenght_test += str(x)
         patient = {'name': 'Marcos Antonia de Freitas testando o Nome'}
+        today = '12/12/2020 15:10'
+        today_day = '12/12/2020'
 
 
         try:
             pdf.add_abbreviated_name(name=patient['name'], pos=(535, 550), camp_name='Patient Name', len_max=26, centralized=True)
 
+            pdf.set_font('Roboto-Mono', 16)
+            pdf.add_datetime(date=today_day, pos=(717, 556), camp_name="Document created date (upper position)", hours=False)
+
             pdf.set_font('Roboto-Mono', 12)
             pdf.add_prescriptions(prescriptions=prescriptions)
+            pdf.add_datetime(date=today, pos=(673, 34), camp_name="Document printed date (Bottom position)")
+            pdf.add_datetime(date=today, pos=(692, 20), camp_name="Document created date (Bottom position)")
         except Exception as error:
             return error
         except:
