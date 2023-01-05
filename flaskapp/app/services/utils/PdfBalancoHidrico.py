185f18b0ff15bec156e5757d2aed420391df2cc9
Ckk3 <contato.gustavomota@gmail.com>
2023-01-04 21:07:11 -0300

    update limits and get fluids variables

--- flaskapp/app/services/utils/PdfBalancoHidrico.py
+++ flaskapp/app/services/utils/PdfBalancoHidrico.py
@@ -13,14 +13,14 @@ class PdfBalancoHidrico(ReportLabCanvasUtils):
         page_size_points = (842, 595)
         super().__init__(canvas_pagesize=page_size_points)
         self.can.setFont('Roboto-Condensed-Bold', 20)
-    
+
 
     def get_output(self) -> PdfWriter:
         return super().get_output()
 
 
-    def get_description_str(self, value:int, description:str) -> str:
-        """Crete description text to add
+    def get_description_values(self, value:int, description:str) -> str:
+        """Create description text and update diurese
 
         Args:
             value (int): _description_
@@ -30,15 +30,22 @@ class PdfBalancoHidrico(ReportLabCanvasUtils):
             str: _description_
         """ 
 
-        start = ''
         if value > 0:
             start = '+'
+            self.administrated_fluids += int(value)
+        else:
+            start = ''
+            self.losts += int(value)
+
+        description = str(description).upper().strip()
+        if description == 'DIURESE':
+            self.diurese += int(value)
         
-        return f'{start}{value}ml ({str(description).upper().strip()})'
+        return f'{start}{value}ml ({description})'
 
 
     def add_description(self, description:str, pos:tuple, camp_name:str) -> None:
-        """Functoin to add descripiton and change color
+        """Function to add description, change color
 
         Args:
             description (str): _description_
@@ -58,10 +65,26 @@ class PdfBalancoHidrico(ReportLabCanvasUtils):
 
     
     def add_fluid_balance(self, balances:list) -> None:
-        
+        """Add all fluid balances to document
+
+        Args:
+            balances (list): _description_
+
+        Returns:
+            _type_: _description_
+        """        
         self.validate_func_args(function_to_verify=self.add_fluid_balance, variables_to_verify={'balances':balances})
-        #maximum balances in first collum
-        FIRST_COLLUM_LIMIT = 13
+
+        if len(balances) > 23:
+            raise Exception('Nao e possivel adicionar mais que 23 fluidos')
+
+        # Create info variables
+        self.administrated_fluids = 0
+        self.losts = 0
+        self.diurese = 0
+
+        # Maximum balances in first collum
+        FIRST_COLLUM_LIMIT = 15
         INCREASE_TO_NEXT_COLLUM = 396
         RECTANGLE_WIDTH = 118
         RECTANGLE_HEIGHT = 25
@@ -81,7 +104,7 @@ class PdfBalancoHidrico(ReportLabCanvasUtils):
             # Add data
             self.add_rectangle(pos=(rectangle_x_pos, rectangle_y_pos), width=RECTANGLE_WIDTH, height=RECTANGLE_HEIGHT)
             self.add_datetime(date=balan['created_at'], pos=(date_x_pos, date_y_pos), camp_name=f'{cont} balance creation date (createdAt)', centralized=True)
-            description_str = self.get_description_str(value=balan['value'],description=balan['description'])
+            description_str = self.get_description_values(value=balan['value'],description=balan['description'])
             self.add_description(description=description_str, pos=(description_x_pos, description_y_pos), camp_name=f'{cont} fluid balance description')
 
             # Update positions
