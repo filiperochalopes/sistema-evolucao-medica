e48e5626af0d4ec31ecae6df82928147e1bb883c
Ckk3 <contato.gustavomota@gmail.com>
2023-01-06 00:32:22 -0300

    removing old code to use new generation

--- flaskapp/app/services/utils/PdfFolhaEvolucao.py
+++ flaskapp/app/services/utils/PdfFolhaEvolucao.py
@@ -62,7 +62,7 @@ class PdfFolhaEvolucao(ReportLabCanvasUtils):
         return professional_info
 
 
-    def add_medical_nursing_evolution_big_squares(self, evolution_description:str, responsible:dict, date:str, evolution_initial_pos:tuple, responsible_initial_pos:tuple, camp_name:str) -> None:
+    def add_medical_nursing_evolution(self, evolution_description:str, responsible:dict, date:str, evolution_initial_pos:tuple, responsible_initial_pos:tuple, camp_name:str) -> None:
         """Add a medical and nursing evolution to the pdf, this function only works to the 2 big squares with data, in order, the first and third square, the other 2 minor nursing evolution will be created by another function
 
         Args:
@@ -76,7 +76,7 @@ class PdfFolhaEvolucao(ReportLabCanvasUtils):
         Returns:
             None
         """
-        self.validate_func_args(function_to_verify=self.add_medical_nursing_evolution_big_squares, variables_to_verify={'evolution_description':evolution_description, 'responsible':responsible, 'responsible_initial_pos':responsible_initial_pos, 'evolution_initial_pos':evolution_initial_pos, 'camp_name':camp_name, 'date':date})
+        self.validate_func_args(function_to_verify=self.add_medical_nursing_evolution, variables_to_verify={'evolution_description':evolution_description, 'responsible':responsible, 'responsible_initial_pos':responsible_initial_pos, 'evolution_initial_pos':evolution_initial_pos, 'camp_name':camp_name, 'date':date})
 
         professional_info = self.create_professional_info(professional=responsible, date=date)
 
@@ -85,20 +85,6 @@ class PdfFolhaEvolucao(ReportLabCanvasUtils):
         self.add_morelines_text(text=professional_info, initial_pos=responsible_initial_pos, decrease_ypos=13, camp_name=f'Informacao do responsavel na evolucao {camp_name}', len_max=99, char_per_lines=49, max_lines_amount=3)
         
         return None
-
-
-    def add_nursing_evolution(self, evolution_description:str, responsible:dict, date:str, evolution_initial_pos:tuple, responsible_initial_pos:tuple, camp_name:str) -> None:
-
-        self.validate_func_args(function_to_verify=self.add_medical_nursing_evolution_big_squares, variables_to_verify={'evolution_description':evolution_description, 'responsible':responsible, 'responsible_initial_pos':responsible_initial_pos, 'evolution_initial_pos':evolution_initial_pos, 'camp_name':camp_name, 'date':date})
-
-        professional_info = self.create_professional_info(professional=responsible, date=date)
-
-        self.add_morelines_text(text=evolution_description, initial_pos=evolution_initial_pos, decrease_ypos=13, camp_name=f'Descricao evolucao {camp_name}', len_max=175, char_per_lines=58)
-
-        self.add_morelines_text(text=professional_info, initial_pos=responsible_initial_pos, decrease_ypos=13, camp_name=f'Informacao do responsavel na evolucao {camp_name}', len_max=99, char_per_lines=49, max_lines_amount=3)
-        
-        return None
-
 
     def add_evolutions(self, evolutions:list) -> None:
         """Add evolutions to pdf
@@ -116,22 +102,7 @@ class PdfFolhaEvolucao(ReportLabCanvasUtils):
         self.validate_func_args(function_to_verify=self.add_evolutions, variables_to_verify={'evolutions':evolutions})
 
         for evo in evolutions:
-            current_category = str(evo['category']).strip().lower()
-            # Add the big squares first
-            if current_category == 'e2':
-            # Add nursing evolution with the same square size than medical evolution
-                self.add_medical_nursing_evolution_big_squares(evolution_description=evo['description'], responsible=evo['professional'], date=evo['created_at'], evolution_initial_pos=(30, 224), responsible_initial_pos=(90, 126), camp_name='de Enfermagem - bloco 2')
-                
-            elif current_category == 'm':
-                # Add medical evolution
-                self.add_medical_nursing_evolution_big_squares(evolution_description=evo['description'], responsible=evo['professional'], date=evo['created_at'], evolution_initial_pos=(30, 498), responsible_initial_pos=(90, 399), camp_name='Medica')
-            elif current_category == 'e1':
-                # Adding nursing evolution
-                self.add_nursing_evolution(evolution_description=evo['description'], responsible=evo['professional'], date=evo['created_at'], evolution_initial_pos=(30, 339), responsible_initial_pos=(90, 285), camp_name='de Enfermagem - Bloco 1')
-            elif current_category == 'e3':
-                self.add_nursing_evolution(evolution_description=evo['description'], responsible=evo['professional'], date=evo['created_at'], evolution_initial_pos=(432, 498), responsible_initial_pos=(490, 445), camp_name='de Enfermagem - Bloco 3')
-            else:   
-                raise Exception(f'A categoria {current_category} nao existe, voce deve escolher M, E1, E2 ou E3, sendo que o numero significa a ordem do bloco, nao se preocupe com espacos ou letras maiusculas')
+            self.add_medical_nursing_evolution(evolution_description=evo['description'], responsible=evo['professional'], date=evo['created_at'], evolution_initial_pos=(30, 224), responsible_initial_pos=(90, 126), camp_name='de Enfermagem - bloco 2')
 
         return None
         
