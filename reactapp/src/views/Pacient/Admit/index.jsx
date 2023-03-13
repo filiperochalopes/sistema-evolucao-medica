9b1b07282d6803d4e9ea81fb6b59b2b731d77722
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-03-13 14:32:59 -0300

    fix: issue 50

--- reactapp/src/views/Pacient/Admit/index.jsx
+++ reactapp/src/views/Pacient/Admit/index.jsx
@@ -143,7 +143,7 @@ const Admit = () => {
     }
     getCep();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formik.values.patient.address.zipCode, statesData.state]);
+  }, [formik.values.patient.address.zipCode, statesData]);
 
   return (
     <Container>
