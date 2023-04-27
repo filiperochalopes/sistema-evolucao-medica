0a4c50635c345966ab0fb0f43c3b102fac60cb75
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-04-27 11:58:56 -0300

    feat: improvement validation in prescriptions

--- reactapp/src/components/Modals/components/ModalsPrescription/components/ModalPrescription/schema.js
+++ reactapp/src/components/Modals/components/ModalsPrescription/components/ModalPrescription/schema.js
@@ -5,33 +5,38 @@ export default Yup.object().shape({
   type: Yup.mixed().required(),
   medicament: Yup.mixed().required(),
   drug: Yup.object()
-    .shape({
-      useMode: Yup.string(ERROR_LABEL),
-      routeAdministration: Yup.mixed(ERROR_LABEL),
-      isAntibiotic: Yup.string(),
-      initialDate: Yup.string().when("isAntibiotic", {
-        is: "atb",
-        otherwise: Yup.string(),
-        then: Yup.string().required(ERROR_LABEL),
-      }),
-      finalDate: Yup.string().when("isAntibiotic", {
-        is: "atb",
-        otherwise: Yup.string(),
-        then: Yup.string().required(ERROR_LABEL),
-      }),
-    })
-    .test("is-drug-valid", "is required", (value, schema) => {
-      if (
-        schema.parent.type?.name !== "drug" ||
-        ((value.finalDate || value.isAntibiotic === "oth") &&
-          (value.initialDate || value.isAntibiotic === "oth") &&
-          value.isAntibiotic &&
-          value.routeAdministration &&
-          value.useMode)
-      ) {
-        return true;
-      }
-      return false;
-    })
+    .when("type", (type, schema) =>
+      type?.name === "drug"
+        ? schema
+            .shape({
+              useMode: Yup.string().required(ERROR_LABEL),
+              routeAdministration: Yup.mixed().required(ERROR_LABEL),
+              isAntibiotic: Yup.string(),
+              initialDate: Yup.string().when("isAntibiotic", {
+                is: "atb",
+                otherwise: Yup.string(),
+                then: Yup.string().required(ERROR_LABEL),
+              }),
+              finalDate: Yup.string().when("isAntibiotic", {
+                is: "atb",
+                otherwise: Yup.string(),
+                then: Yup.string().required(ERROR_LABEL),
+              }),
+            })
+            .test("is-drug-valid", "is required", (value, schema) => {
+              if (
+                schema.parent.type?.name !== "drug" ||
+                ((value.finalDate || value.isAntibiotic === "oth") &&
+                  (value.initialDate || value.isAntibiotic === "oth") &&
+                  value.isAntibiotic &&
+                  value.routeAdministration &&
+                  value.useMode)
+              ) {
+                return true;
+              }
+              return false;
+            })
+        : schema
+    )
     .required(),
 });
