a43204d5270361a5d28fed1b0d1f105538203c26
Filipe Lopes <contato@filipelopes.me>
2023-05-28 16:25:20 -0300

    feat: redirect expired user to login page

--- reactapp/src/routes/PrivateRouter.jsx
+++ reactapp/src/routes/PrivateRouter.jsx
@@ -1,8 +1,14 @@
+import { useContextProvider } from "services/Context";
+
 const { Navigate, useLocation } = require("react-router-dom");
 
 const PrivateRouter = ({ children }) => {
+  const { decodedJWT } = useContextProvider();
   const location = useLocation();
-  if (location.pathname !== "/" && !localStorage.getItem("token")) {
+  if (
+    (location.pathname !== "/" && !localStorage.getItem("token")) ||
+    new Date() > new Date(decodedJWT?.exp * 1000)
+  ) {
     return <Navigate to="/" replace />;
   }
   return children;
