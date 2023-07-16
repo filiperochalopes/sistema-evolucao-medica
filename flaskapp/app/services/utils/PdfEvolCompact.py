92cce69bfa880e95753c95725ab0b7d5b450a49d
Ckk3 <contato.gustavomota@gmail.com>
2023-07-16 20:40:47 -0300

    create_professional_info_text checks only first prof_category char

--- flaskapp/app/services/utils/PdfEvolCompact.py
+++ flaskapp/app/services/utils/PdfEvolCompact.py
@@ -60,7 +60,7 @@ class PdfEvolCompact(ReportLabCanvasUtils):
             date_object.minute,
         )
 
-        document_category = "CRM" if category.lower() == "m" else "COREN"
+        document_category = "CRM" if category.lower()[0] == "m" else "COREN"
 
         prof_info = (
             f"{str(name).strip()} {document_category} {str(document)}"
